4dc3eb328130b956118112ad240757c4f874654b
dinaaprilia <132429093+dinaaprilia@users.noreply.github.com>
2025-07-02 09:48:03 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'railway'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
