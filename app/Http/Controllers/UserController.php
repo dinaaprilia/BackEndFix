0c7d3199a854412f7bd859709990ddd5b0f7f441
dinaaprilia <132429093+dinaaprilia@users.noreply.github.com>
2025-07-04 23:31:43 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
-use App\Models\absensi;
+use App\Models\Absensi;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
